b639adf2f56c7224830e46609eb14f59df19b88c
Joonhyung Cho <choms46@me.com>
2018-10-31 02:30:23 +0900

    Updates

---
+++ precache-manifest.55268a984616428506acdd1c2bf05c35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9575f5892da251c03bed",
+    "url": "/faceMoji/static/js/runtime~main.9575f589.js"
+  },
+  {
+    "revision": "109fed869d010c84512d",
+    "url": "/faceMoji/static/js/main.109fed86.chunk.js"
+  },
+  {
+    "revision": "0c206070c840bf2ff39c",
+    "url": "/faceMoji/static/js/1.0c206070.chunk.js"
+  },
+  {
+    "revision": "109fed869d010c84512d",
+    "url": "/faceMoji/static/css/main.d45db8d1.chunk.css"
+  },
+  {
+    "revision": "4dde6a52f90cc63af713b24bfc22016a",
+    "url": "/faceMoji/index.html"
+  }
+];
